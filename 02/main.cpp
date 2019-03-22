6bacb5505c21edf2a35fb4ee170f13d7afde3d5e
GeorgyFirsov <gfirsov007@gmail.com>
2019-03-23 00:48:06 +0300

    Fixed spaces (I hope)

--- 02/main.cpp
+++ 02/main.cpp
@@ -7,7 +7,7 @@ using std::out_of_range;
 
 int main() 
 {
-	RunAllTests();
+    RunAllTests();
     Matrix matr(6,7);
     for(std::size_t i = 0; i < matr.getRows(); i++)
         for(std::size_t j = 0; j < matr.getColumns(); j++) matr[i][j] = rand() % 10;
@@ -15,5 +15,5 @@ int main()
     matr *= 2;
     cout << "\nMatrix:\n" << matr;
     cout << "\nConst matrix:\n" << cmatr;
-	return 0;
+    return 0;
 }
