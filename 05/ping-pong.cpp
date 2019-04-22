07dd0429c88d200789c8060bf02095ca47ada736
Georgy Firsov <gfirsov007@gmail.com>
2019-04-22 10:27:28 +0300

    Homework 05 done

---
+++ 05/ping-pong.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+constexpr auto COUNT = 500000;
+
+using std::cout;
+using std::endl;
+
+std::mutex m1;
+std::mutex m2;
+
+void ping()
+{
+	for(int i = 0; i < COUNT; i++)
+	{
+		m1.lock();
+		cout << "ping" << endl;
+		m2.unlock();
+	}
+}
+
+void pong()
+{
+	for(int i = 0; i < COUNT; i++)
+	{
+		m2.lock();
+		cout << "pong" << endl;
+		m1.unlock();
+	}
+}
+
+int main()
+{
+	std::thread t_ping(pint); m2.lock();
+	std::thread t_pong(pong);
+	t_ping.join();
+	t_pong.join();
+	return 0;
+}
